3a69e16908657e0445901780dad2ac4c18769ce9
ryans-git <ryan_hennings@amat.com>
2019-08-02 12:18:27 -0700

    fully working program

--- guessinggame.sh
+++ guessinggame.sh
@@ -3,12 +3,14 @@
 
 main () 
 {
+   count=$(get_file_count)
+
    local correct=false
 
    while [[ $correct = false ]] ; do
       echo "Enter your guess"
       read guess
-      if [[ $guess = "2" ]]; then 
+      if [[ $guess = $count ]]; then 
          echo "Correectdemundo" 
          correct=true
       else 
@@ -17,7 +19,15 @@ main ()
       fi
    done
 
-   echo "done"
+   echo "Bye Bye \o"
+}
+
+# Assuming the prompt means find FILES exclusively; not Dirs also
+# Logic: list files; exclude lines starting with d (dirs) and t (total); get line count
+get_file_count () 
+{
+   count=$(ls -l |egrep -v "(^d|^t)" |wc -l)
+   echo "$count"
 }
 
 
